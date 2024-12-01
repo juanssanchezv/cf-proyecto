0fa49d833ecbc70f577141562092bd03e1a64822
juanssanchezv <js.sanchez130@gmail.com>
2024-12-01 13:09:50 -0500

    Add dotenv dependency

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
-
-  logLevel: 'info',
+  logLevel: "info",
 
   plugins: [reactRefresh()],
   server: {
     host: process.env.VITE_HOST || null,
     port: process.env.VITE_PORT || null,
     hmr: {
-      clientPort: process.env.VITE_CLIENT_PORT || null
+      clientPort: process.env.VITE_CLIENT_PORT || null,
     },
     proxy: {
-      '^/api': {
-        target: 'http://api',
-        changeOrigin: true
-      }
-    }
-  }
-})
+      "^/api": {
+        target: "http://api",
+        changeOrigin: true,
+      },
+    },
+  },
+});
